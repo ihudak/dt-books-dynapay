257924c16bc3410e75eaf79b9e53862ce097aa90
Ivan Gudak <ihudak@gmail.com>
2023-01-30 22:32:08 +0100

    Configurable workloader

--- src/main/java/com/dynatrace/dynapay/controller/DynaPayController.java
+++ src/main/java/com/dynatrace/dynapay/controller/DynaPayController.java
@@ -1,6 +1,8 @@
 package com.dynatrace.dynapay.controller;
 
 import com.dynatrace.dynapay.model.DynaPay;
+import com.dynatrace.dynapay.repository.ConfigRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,24 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/")
-public class DynaPayController {
-    @Value("${added.workload.cpu}")
-    private long cpuPressure;
-    @Value("${added.workload.ram}")
-    private int memPressureMb;
-    @Value("${payment.failure.probability}")
-    private int paymentFailureProbability;
+@RequestMapping("/api/v1/dynapay")
+public class DynaPayController extends HardworkingController {
+    @Autowired
+    ConfigRepository configRepository;
 
 
     // make a payment
-    @PostMapping("/dynapay")
+    @PostMapping("")
     public DynaPay createPayment(@RequestBody DynaPay dynaPay) {
         simulateHardWork();
 
         double rand = Math.random();
 
-        if (rand >= (double) paymentFailureProbability / 100.0) {
+        if (rand >= getPercentFailure() / 100.0) {
             // successful payment
             dynaPay.setSucceeded(true);
             dynaPay.setMessage("OK");
@@ -37,23 +35,8 @@ public class DynaPayController {
         return dynaPay;
     }
 
-    private void simulateHardWork() {
-        int arraySize = (int)((long)this.memPressureMb * 1024L * 1024L / 8L);
-        if (arraySize < 0) {
-            arraySize = Integer.MAX_VALUE;
-        }
-        long[] longs = new long[arraySize];
-        int j = 0;
-        for(long i = 0; i < this.cpuPressure; i++, j++) {
-            j++;
-            if (j >= arraySize) {
-                j = 0;
-            }
-            try {
-                if (longs[j] > Integer.MAX_VALUE) {
-                    longs[j] = (long) Integer.MIN_VALUE;
-                }
-            } catch (Exception ignored) {};
-        }
+    @Override
+    public ConfigRepository getConfigRepository() {
+        return configRepository;
     }
 }
